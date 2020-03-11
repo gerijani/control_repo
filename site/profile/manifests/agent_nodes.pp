18c8def2c50979240c04e654de0c81195caac8d3
gerijani <18419444+gerijani@users.noreply.github.com>
2020-03-12 00:10:46 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profiles::agent_nodes {
     include dockeragent
-    dockeragent::node {'web.puppet.vm'}
-    dockeragent::node {'db.puppet.vm'}
+    dockeragent::node {'web.puppet.vm':}
+    dockeragent::node {'db.puppet.vm':}
 }

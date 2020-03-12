8a99871cf9de0efa9d865c26de2754bc9c553bbd
gerijani <gerijani@gmail.com>
2020-03-12 20:40:29 +0100

    TYPO 2

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,7 +7,7 @@ class minecraft {
 		source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar'
 	}
 	package {'java':
-		ensuse => present,
+		ensure => present,
 	}
 	file {'/opt/minecraft/elua.txt':
 		ensure => file,

dd0af620a2745655445bb48bbfb55aef26f26fa8
gerijani <gerijani@gmail.com>
2020-03-12 14:34:05 +0100

    Using fact

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,17 @@
 node default {
   file {'/root/README':
     ensure  => 'file',
-    content => 'This is a readme\n
-    and this is for test',
+    content => 'This is a readme
+    and only for test',
     owner   => 'root',
   }
 }
 node 'centos-7.atlantis.local' {
   include role::master_server
+  file { '/root/README':
+	ensure => file,
+	content => $fqdn,
+  }
 }
 
 node /^web/{
